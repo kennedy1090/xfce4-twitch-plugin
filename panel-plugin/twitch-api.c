11f9f1b44c440c64369166ad04695b493a94daf3
Will Kennedy <willmlb@sbcglobal.net>
2019-08-01 14:56:53 -0400

    remove redundant get data

--- panel-plugin/twitch-api.c
+++ panel-plugin/twitch-api.c
@@ -81,12 +81,13 @@ static GdkPixbuf* curl_read_image(TwitchApi* api, gchar *url) {
     g_object_unref(G_OBJECT(loader));
     return pixbuf;
 }
-static gboolean twitch_verify_response(json_object *json) {
+static gboolean twitch_get_data(json_object *json, json_object **data) {
+    json_object *error, *message;
     if (!json) {
         g_error("Invalid JSON object");
         return FALSE;
     }
-    if(!json_object_object_get_ex(json, "data", &data)) {
+    if(!json_object_object_get_ex(json, "data", data)) {
         json_object_object_get_ex(json, "error", &error);
         json_object_object_get_ex(json, "message", &message);
         g_error("Twitch API Error: %s", json_object_get_string(error));
@@ -103,8 +104,7 @@ static gboolean twitch_init_user_id (TwitchApi* api) {
     const gchar *id_c;
     url = g_strconcat(TWITCH_API_USERS, "?login=", api->user.name, NULL);
     json = curl_fetch_json(api, url);
-    if(!twitch_verify_response(json)) return FALSE;
-    json_object_object_get_ex(json, "data", &data);
+    if(!twitch_get_data(json, &data)) return FALSE;
     user = json_object_array_get_idx(data, 0);
     json_object_object_get_ex(user, "id", &id);
     id_c = json_object_get_string(id);
@@ -128,9 +128,8 @@ static gboolean twitch_update_following (TwitchApi* api) {
             url = g_strconcat(url_base, "&after=", json_object_get_string(cursor), NULL);
         } else url = g_strdup(url_base);
         json = curl_fetch_json(api, url);
-        if(!twitch_verify_response(json)) return FALSE;
+        if(!twitch_get_data(json, &data)) return FALSE;
         json_object_object_get_ex(json, "total", &total);
-        json_object_object_get_ex(json, "data", &data);
         json_object_object_get_ex(json, "pagination", &pagination);
         json_object_object_get_ex(pagination, "cursor", &cursor);
         count = json_object_get_int(total);
@@ -182,7 +181,7 @@ static gboolean twitch_update_user_data(TwitchApi *api) {
     loop.prefix = prefix;
     g_hash_table_foreach(api->following, copy_ids, &loop);
     json = curl_fetch_json(api, url_final);
-    if(!twitch_verify_response(json)) return FALSE;
+    if(!twitch_get_data(json, &data)) return FALSE;
     for (gsize i = 0; i < json_object_array_length(data); i++) {
         json_object_object_get_ex(json_object_array_get_idx(data, i), "profile_image_url", &pfp);
         json_object_object_get_ex(json_object_array_get_idx(data, i), "id", &id);
@@ -227,7 +226,6 @@ gboolean twitch_update_users(TwitchApi *api) {
 gboolean twitch_update_status (TwitchApi *api) {
     gchar *offset, *url, *url_final;
     json_object *data, *type, *id, *json, *title;
-    json_object *error, *message;
     id_loop loop;
     TwitchUser *user;
     gsize size_total;
@@ -244,7 +242,7 @@ gboolean twitch_update_status (TwitchApi *api) {
     g_hash_table_foreach(api->following, copy_ids, &loop);
     g_hash_table_foreach(api->following, set_false, NULL);
     json = curl_fetch_json(api, url_final);
-    if(!twitch_verify_response(json)) return FALSE;
+    if(!twitch_get_data(json, &data)) return FALSE;
     for (gsize i = 0; i < json_object_array_length(data); i++) {
         json_object_object_get_ex(json_object_array_get_idx(data, i), "user_id", &id);
         json_object_object_get_ex(json_object_array_get_idx(data, i), "type", &type);
